8e0485208da9bd6c0f2b88a4251daff183ae065e
Mehmet Arslan <marslan.developer@gmail.com>
2022-09-18 18:06:33 +0300

    done

---
+++ app.js
@@ -0,0 +1,22 @@
+//? Selectors
+
+const input = document.querySelector(".form-control");
+const button = document.querySelector(".btn");
+const result = document.querySelector(".second");
+
+//? button event handler
+button.addEventListener("click", () => {
+  if (!input.value) {
+    alert("Please write a sentence");
+  } else {
+    const vowelArr = ["a", "e", "ı", "i", "o", "ö", "u", "ü"];
+    const inputArr = input.value.toLowerCase().split("");
+    let counter = 0;
+
+    for (let letter of inputArr) {
+      vowelArr.includes(letter) && counter++;
+
+      result.innerHTML = `<p>There is/are ${counter} vowel letters in the <span style="font-weight:600"=>"${input.value}"</span>sentence </p>`;
+    }
+  }
+});
